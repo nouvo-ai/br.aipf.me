0514bb50d6ac7b69e0ee09340b1088da7c91c17c
c2lv <hyeonjun7@naver.com>
2023-09-13 12:30:29 +0900

    ss

--- netlify/functions/myFunctions.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-    const { S3_REGION } = process.env;
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: `S3_REGION: ${S3_REGION}` }),
-    };
-};
